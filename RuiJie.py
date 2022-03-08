8f984191565d43146e1d473b27831e4cf2cbe909
Yu Wang <ywang_wnlo@qq.com>
2022-03-08 10:28:13 +0800

    改用 ping 来判断网络状态

--- RuiJie.py
+++ RuiJie.py
@@ -3,6 +3,7 @@
 
 import requests
 from bs4 import BeautifulSoup
+import os
 import sys
 import time
 import json
@@ -10,27 +11,29 @@ import json
 class RuiJie(object):
     def __init__(self, userId, password):
         self._test_time = 60
-        self._status = False
         self._userId = userId
         self._password = password
 
+    def _ping(self, host):
+        cmd = "ping {} 2 {} > ping.log".format(
+            "-n" if sys.platform.lower() == "win32" else "-c",
+            host
+        )
+        return False if os.system(cmd) else True
+
     def _check_status(self):
-        test_url = "http://1.1.1.1"
+        return self._ping("202.114.0.242") or self._ping("223.5.5.5")
+
+    def _reconnection(self):
+        test_url = "http://192.168.1.1"
         page = requests.get(test_url)
-        # page.encoding = "utf8"
         soup = BeautifulSoup(page.text, "html5lib")
-        meta = soup.select_one("head > meta")
-        self._status = (meta is not None)
-        if self._status:
-            return
         time_string = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         print("[Log] [%s]" % (time_string), end=" ")
         print("锐捷掉线，尝试重连", end=" ")
         script = soup.select_one("script")
         self._referer = script.string.split("\'")[1]
         self._origin = self._referer.split("/eportal/")[0]
-
-    def _reconnection(self):
         url = self._origin + "/eportal/InterFace.do?method=login"
         data = {
             "userId": self._userId,
@@ -55,8 +58,7 @@ class RuiJie(object):
 
     def run(self):
         while(True):
-            self._check_status()
-            if self._status:
+            if self._check_status():
                 time.sleep(self._test_time)
             else:
                 self._reconnection()
