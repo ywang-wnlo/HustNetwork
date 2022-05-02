661b5ba20e8d5b6a05abc40cd6d0744a5b50c5a0
Yu Wang <ywang_wnlo@qq.com>
2022-05-02 12:45:17 +0800

    美化代码，改为中文文档

--- HustNetwork.py
+++ HustNetwork.py
@@ -8,14 +8,17 @@ import time
 
 import requests
 
+HUST_DNS = "202.114.0.242"
+OTHER_DNS = "223.5.5.5"
 
-class RuiJie(object):
+class HustNetwork(object):
     def __init__(self, userId, password):
         self._test_time = 60
         self._userId = userId
         self._password = password
 
     def _ping(self, host):
+        # 利用 ping 判断网络状态
         cmd = "ping {} 2 {} > ping.log".format(
             "-n" if sys.platform.lower() == "win32" else "-c",
             host
@@ -23,22 +26,26 @@ class RuiJie(object):
         return False if os.system(cmd) else True
 
     def _check_status(self):
-        return self._ping("202.114.0.242") or self._ping("223.5.5.5")
+        # 依次 ping 校园网 DNS 和 阿里云 DNS
+        return self._ping(HUST_DNS) or self._ping(OTHER_DNS)
 
     def _reconnection(self):
         time_string = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         print("[Log] [%s]" % (time_string), end=" ")
         print("锐捷掉线，尝试重连", end=" ")
 
+        # 通过 http 的网站进行跳转
         test_url = "http://192.168.1.1"
         response = requests.get(test_url)
         response.encoding = 'utf8'
 
+        # 获取跳转链接
         href = re.findall(r"href='(.+)'", response.text)
         referer = href[0]
         origin = referer.split("/eportal/")[0]
         url = origin + "/eportal/InterFace.do?method=login"
 
+        # 组成 post 数据
         data = {
             "userId": self._userId,
             "password": self._password,
@@ -49,6 +56,7 @@ class RuiJie(object):
             "validcode": ""
         }
 
+        # 校园网认证
         headers = {
             "Host": origin.split("://")[1],
             "Origin": origin,
@@ -56,22 +64,24 @@ class RuiJie(object):
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
         }
         response = requests.post(url, data=data, headers=headers)
+
+        # 打印响应状态
         response.encoding = response.apparent_encoding
         result = response.json()
         print(result["result"], result["message"])
 
     def run(self):
         while(True):
-            time.sleep(self._test_time)
             if not self._check_status():
                 self._reconnection()
+            time.sleep(self._test_time)
 
 
 if __name__ == "__main__":
-    ruijie = RuiJie(sys.argv[1], sys.argv[2])
+    hustNetwork = HustNetwork(sys.argv[1], sys.argv[2])
     while(True):
         try:
-            ruijie.run()
+            hustNetwork.run()
         except Exception as e:
             time_string = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
             print("[Exception] [%s]" % (time_string), end=" ")
--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-# KeepLogInRuiJie
+# HustNetwork
 
-## Function:
+## 功能
 
-Make ruijie always keep log in with Web Authentication on Windows/Linux
+无库依赖，自动认证华科校园网，并支持断线重连
 
-## Use:
+## 使用
 
-Wireless/Wired Network:
+无需在路由器上，任何（通过通过路由器的）接入校园网的设备均可运行
 
 ```bash
-python3 RuiJie.py {UserId} {Password}
+python3 HustNetwork.py {校园网账号} {校园网密码}
 ```
+
+程序需保持一直运行，推荐使用 screen 或者 systemctl 配置成 service 挂在后台
